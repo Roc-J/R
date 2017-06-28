4372bbbe185876b4831934496ec98d5cc4ace8be
Roc-J <646481338@qq.com>
2017-06-28 23:00:22 +0800

    Vector learn

---
+++ Vector.R
@@ -0,0 +1,18 @@
+# Vector
+
+x <- vector("character", length = 10)
+
+x1 <- 1:4
+
+x2 <- c(1,2,3,4)
+
+x3 <- c(TRUE, 10 ,"a")
+
+x4 <- c("a", "b", "c")
+
+as.numeric(x4)
+as.numeric(x)
+as.character()
+
+class(x1)
+names(x1) <- c("a","b","c","d")
