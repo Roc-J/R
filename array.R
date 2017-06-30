0a23b86cac8596eac46f3506d52b3f44bd79a860
Roc-J <646481338@qq.com>
2017-06-30 11:33:28 +0800

    modify the array.R

--- array.R
+++ array.R
@@ -9,3 +9,9 @@ x1 <- array(1:24,dim = c(2,3,4))
 l <- list("a", 2, 10L, 3+4i, TRUE)
 
 l2 <- list(a=1, b=2, c=3)
+
+l3 <- list(c(1,2,3), c(4,5,6,7))
+
+x <- matrix(1:6, nrow = 2, ncol = 3)
+
+dimnames(x) <- list(c("a","b"), c("c","d","e"))
